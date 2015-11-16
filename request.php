5d03535e4dd107503e8d44e3689eb4aa46499a58
oforbes <odaine.forbes@yahoo.com>
2015-11-16 09:50:56 -0500

    Create request.php

---
+++ request.php
@@ -0,0 +1,20 @@
+<?php
+// For PHP 5 and up
+
+# accept a term (keyword)
+# respond with a value
+$query = $_REQUEST['q'];
+
+
+
+$definition = [
+    "definition" => "a statement of the exact meaning of a word, especially in a dictionary.",
+    "bar" => "a place that sells alcholic beverages",
+    "ajax" => "technique which involves the use of javascript and xml"
+];
+
+print $query;
+print "<br>";
+print($definition[$query]);
+
+?>
